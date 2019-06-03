a0f03997d993ba48e37ac52491dfa2a5608b8291
Harm <Harm.van.verseveld@hva.nl>
2019-06-03 22:42:06 +0200

    have tried jade but going back to handlebars

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Harms buckettest', condition : false });
 });
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET users details. */
+router.get('/users/detail', function(req, res, next) {
+  res.send('detail');
+});
+
 
 module.exports = router;
