5990d24eb137714928851558e85f0176941b3dad
Harm <Harm.van.verseveld@hva.nl>
2019-06-04 08:49:37 +0200

    Added errors and validators in form

--- routes/index.js
+++ routes/index.js
@@ -39,13 +39,39 @@ var people =
 
 }
 /* GET home page. */
+
+// router.get('/', function(req, res, next) {
+//   res.render('index',{
+//     content : "Dit is content",
+//     people : people,
+//     profile : profile,
+//     title : "Home",
+//     title: 'Form Validation', success: req.session.success, errors: req.session.errors
+// });
+// req.session.errors = null;
+// });
+
+
+//get homepage test
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{
-    content : "Dit is content",
-    people : people,
-    profile : profile,
-    title : "Home"
+  res.render('index', {     people : people, profile : profile, title: 'Form Validation', success: req.session.success, errors: req.session.errors });
+  req.session.errors = null;
 });
+
+router.post('/submit', function(req, res, next) {
+  req.check('email', 'Invalid email address').isEmail();
+  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    req.session.errors = errors;
+    req.session.success = false;
+  } else {
+    req.session.success = true;
+  }
+  res.redirect('/');
 });
 
 //test
