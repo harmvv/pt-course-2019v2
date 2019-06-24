b08b4e9cc1ec7ee46b167d9abf64f08d3db563b9
Harm <Harm.van.verseveld@hva.nl>
2019-06-24 16:44:49 +0200

    added form progressive enhancment

---
+++ public/javascripts/buckettest.js
@@ -0,0 +1,76 @@
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console':0*/
+
+var classOne    = document.querySelector(".tab1");
+var classTwo    = document.querySelector(".tab2");
+var classThree  = document.querySelector(".tab3");
+
+
+var allClasses = [classOne, classTwo, classThree];
+
+allClasses.forEach(function(el) {
+  el.classList.remove("tabnojs")
+})
+
+
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  
+    document.getElementById("prevBtn").style.display = "inline";
+  
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Verstuur";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Volgende";
+  }
+  // ... and run a function that displays the correct step indicator:
+ 
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Exit the function if any field in the current tab is invalid:
+  if (n == 1 && !validateForm()) return false;
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n;
+  // if you have reached the end of the form... :
+  if (currentTab >= x.length) {
+    //...the form gets submitted:
+    document.getElementById("regForm").submit();
+    return false;
+  }
+  // Otherwise, display the correct tab:
+  showTab(currentTab);
+}
+
+function validateForm() {
+  // This function deals with validation of the form fields
+  var x, y, i, valid = true;
+  x = document.getElementsByClassName("tab");
+  y = x[currentTab].getElementsByTagName("input");
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
+    // If a field is empty...
+    if (y[i].value == "") {
+      // add an "invalid" class to the field:
+      y[i].className += " invalid";
+      // and set the current valid status to false:
+      valid = false;
+    }
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName("step")[currentTab].className += " finish";
+  }
+  return valid; // return the valid status
+}
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,6 +7,8 @@
 
 console.log("pino");
 
+
+
 function openMenu() {
   console.log("hello2")
   var x = document.querySelector(".hamburgernav");
@@ -28,35 +30,3 @@ hamburgericon.addEventListener("click", openMenu);
 close.addEventListener("click", openMenu);
 
 
-// users
-
-
-//Retrieve the template data from the HTML .
-var template = $('#matchestemplate').html();
-
-
-// function calculateType(event) {
-//   // event.preventDefault();
-//   // console.log("test submit")
-//   var option1 = document.querySelector('input[name="vacation"]:checked').value
-//   var option2 = document.querySelector('input[name="preferences"]').value
-//   var option3 = document.querySelector('input[name="date"]:checked').value
-//   var outcomeInput = document.querySelector('input[name="outcomeInput"]')
-//   var outcome = Number(option1) + Number(option2) + Number(option3);
-//   console.log("werkt dit")
-//   console.log("test submit" + option1 + " " + option2 + " " + option3);
-//   console.log(outcome);
-//   var type = "";
-//   if (outcome < 12) {
-//     type = "Relaxer";
-//   } else if (outcome > 11 && outcome < 20) {
-//     type = "Ontdekker"
-//   } else {
-//     type = "Avonturier"
-//   }
-//   console.log(type)
-//   outcomeInput.value = type
-// };
-
-// var bucketForm = document.querySelector(".buckettest")
-// bucketForm.addEventListener("submit", calculateType);
